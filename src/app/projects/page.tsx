281413f18b46975c2cbb22c304cf5ccf41588fc9
Mohammed Ahmed <mahmed0x1@gmail.com>
2023-07-26 01:00:02 +0900

    projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,61 @@
+"use client";
+
+import Slider from "react-slick";
+
 import Navigation from "@/components/navigation";
 
 import styles from "./page.module.css";
+import "node_modules/slick-carousel/slick/slick.css";
+import "node_modules/slick-carousel/slick/slick-theme.css";
 
-export default function Home() {
+interface NavigationProps {}
+
+const Projects: React.FC<NavigationProps> = ({}) => {
+  const settings = {
+    dots: true,
+    fade: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className={styles.root}>
       <Navigation active="projects" />
       <div className={styles.main}>
-        <h1>projects</h1>
+        <div className={styles.header}>
+          <h1>Portfolio</h1>
+          <img src="/images/paint.png" />
+        </div>
+        <div className={styles.projects}>
+          <div className={styles.project}>
+            <div className={styles.gallery}>
+              <Slider {...settings}>
+                <img src="/projects/1.png" />
+
+                <img src="/projects/2.png" />
+              </Slider>
+            </div>
+            <div className={styles.description}>
+              <h2>ChatApp</h2>
+              <p>This is a sample chat app that i made using react native</p>
+              <div>
+                <span>React Native</span>
+                <span>Redux</span>
+              </div>
+              <div>
+                <a>
+                  View Code <i className="fa-brands fa-github-alt"></i>
+                </a>
+                <a>
+                  Live Demo <i className="fa-solid fa-up-right-from-square"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default Projects;
